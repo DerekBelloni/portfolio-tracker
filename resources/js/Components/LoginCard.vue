ede7b3c68b31d6daf82c83b6a50ea9717c5dbba5
Derek Belloni <derekbelloni10@gmail.com>
2024-10-26 21:39:02 -0600

    building login component

---
+++ resources/js/Components/LoginCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <Card
+     pt:root:class="border border-gray-300 border-solid rounded-lg"
+     pt:title:class="flex justify-center my-6"
+     >
+        <template #title>
+            <span class="text-2xl text-gray-700">Login</span>
+        </template>
+        <template #content>
+            <div class="m-12">
+                <FloatLabel variant="on">
+                    <InputText id="username" type="text" pt:root:class="border border-solid rounded-lg bg-gray-50"></InputText>
+                    <label for="username">Username</label>
+                </FloatLabel>
+            </div>
+        </template>
+    </Card>
+</template>
+
+<script setup>
+import Card from 'primevue/card';
+import InputText from 'primevue/inputtext';
+import FloatLabel from 'primevue/floatlabel';
+</script>
