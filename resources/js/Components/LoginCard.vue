8c830e94b8d6b7aea209c0301c0ad03df4746749
Derek Belloni <derekbelloni10@gmail.com>
2024-10-26 22:55:19 -0600

    general styling implemented for the login card component

--- resources/js/Components/LoginCard.vue
+++ resources/js/Components/LoginCard.vue
@@ -1,17 +1,29 @@
 <template>
     <Card
-     pt:root:class="border border-gray-300 border-solid rounded-lg"
-     pt:title:class="flex justify-center my-6"
+     pt:root:class="border border-gray-800 border-solid rounded-lg"
+     pt:title:class="flex justify-center mt-8"
      >
         <template #title>
-            <span class="text-2xl text-gray-700">Login</span>
+            <span class="text-2xl text-gray-200">Login</span>
         </template>
         <template #content>
-            <div class="m-12">
+            <div class="m-12 space-y-8">
                 <FloatLabel variant="on">
-                    <InputText id="username" type="text" pt:root:class="border border-solid rounded-lg bg-gray-50"></InputText>
+                    <InputText v-mode="username" id="username" type="text" class="border border-solid rounded-lg bg-gray-50" size="large" fluid></InputText>
                     <label for="username">Username</label>
                 </FloatLabel>
+                <FloatLabel variant="on">
+                    <Password v-model="password" id="password" class="border border-solid rounded-lg bg-gray-50" size="large" fluid></Password>
+                    <label for="password">Password</label>
+                </FloatLabel>
+                <div class="flex justify-center">   
+                    <a href="" class="text-gray-500 hover:text-emerald-500 cursor-pointer">Click here to sign up</a>
+                </div>
+            </div>
+        </template>
+        <template #footer>
+            <div class="flex gap-4 mt-1">
+                <Button label="Submit" class="w-full border border-gray-200" />
             </div>
         </template>
     </Card>
@@ -21,4 +33,12 @@
 import Card from 'primevue/card';
 import InputText from 'primevue/inputtext';
 import FloatLabel from 'primevue/floatlabel';
+import Button from 'primevue/button';
+import Password from 'primevue/password';
+import { ref } from 'vue';
+
+const username = ref(null);
+const password = ref(null);
+
+
 </script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,12 @@
 import '../css/app.css';
 import './bootstrap';
-
+    
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 import PrimeVue from "primevue/config";
+import Aura from '@primevue/themes/aura';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -20,7 +21,7 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
-            .use(PrimeVue, { unstyled: true })
+            .use(PrimeVue, { unstyled: false, theme: { preset: Aura } })
             .mount(el);
     },
     progress: {
