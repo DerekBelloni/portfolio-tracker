101428874826584e7290af46f74ce8c70565756a
Derek Belloni <derekbelloni10@gmail.com>
2024-10-27 00:18:39 -0600

    implemented toggle between login and registration cards

--- resources/js/Components/RegistrationCard.vue
+++ resources/js/Components/RegistrationCard.vue
@@ -4,7 +4,7 @@
      pt:title:class="flex justify-center mt-8"
      >
         <template #title>
-            <span class="text-2xl text-gray-200">Login</span>
+            <span class="text-2xl text-gray-200">Register</span>
         </template>
         <template #content>
             <div class="m-12 space-y-8">
@@ -13,16 +13,22 @@
                     <label for="username">Username</label>
                 </FloatLabel>
                 <FloatLabel variant="on">
+                    <InputText v-mode="username" id="username" type="text" class="border border-solid rounded-lg bg-gray-50" size="large" fluid></InputText>
+                    <label for="username">Email</label>
+                </FloatLabel>
+                <FloatLabel variant="on">
                     <Password v-model="password" id="password" class="border border-solid rounded-lg bg-gray-50" size="large" fluid></Password>
                     <label for="password">Password</label>
                 </FloatLabel>
-                <div class="flex justify-center">   
-                    <a @click="toggleActiveCard()" class="text-gray-500 hover:text-emerald-500 cursor-pointer">Click here to sign up</a>
-                </div>
+                <FloatLabel variant="on">
+                    <Password v-model="password" id="password" class="border border-solid rounded-lg bg-gray-50" size="large" fluid></Password>
+                    <label for="password">Confirm Password</label>
+                </FloatLabel>
             </div>
         </template>
         <template #footer>
             <div class="flex gap-4 mt-1">
+                <Button @click="toggleActiveCard()" label="Cancel" severity="secondary" outlined class="w-full" />
                 <Button label="Submit" class="w-full border border-gray-200" />
             </div>
         </template>
@@ -35,7 +41,7 @@ import InputText from 'primevue/inputtext';
 import FloatLabel from 'primevue/floatlabel';
 import Button from 'primevue/button';
 import Password from 'primevue/password';
-import { defineEmits, ref } from 'vue';
+import { ref, defineEmits } from 'vue';
 
 const username = ref(null);
 const password = ref(null);
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,24 +1,33 @@
 <template>
-    <div class="login-card-container">
-        <div class="login-card">
-            <LoginCard/>
+    <div class="welcome-card-container">
+        <div class="welcome-card">
+            <component :is="!registrationActive ? LoginCard : RegistrationCard" @toggleActive="toggleActive()"/>
         </div>
     </div>
 </template>
 
 <script setup>
 import LoginCard from '../Components/LoginCard.vue'
+import RegistrationCard from '@/Components/RegistrationCard.vue';
+import { reactive, ref, onBeforeMount, shallowRef } from 'vue';
+
+const registrationActive = ref(false);
+
+function toggleActive(toggleTo) {
+    registrationActive.value = !registrationActive.value;
+}
+
 </script>
 
 <style lang="css" scoped>
-    .login-card-container {
+    .welcome-card-container {
         display: flex;
         min-height: 100vh;
         align-items: center;
         justify-content: center;
     }
 
-    .login-card {
+    .welcome-card {
         max-width: 35%;
     }
 </style>
